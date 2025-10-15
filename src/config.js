f8bb38ed4abd672d91ab436b0910ea1c461e2a98
Reinhard Holl <xeroo125@gmail.com>
2025-10-15 11:19:10 +0200

    Implement some env switching

--- src/config.js
+++ src/config.js
@@ -1,12 +1,14 @@
+import { AURA_ENV } from '@env';
+
 const config = {
   development: {
     API_URL: 'https://dev-panic.aura.services/panic-api/v2',
   },
-  production: {
-    API_URL: 'https://panic.aura.services/panic-api/v2',
+  staging: {
+    API_URL: 'https://staging-panic.aura.services/panic-api/v2',
   },
 };
 
-const ENV = process.env.NODE_ENV || 'development';
+const ENV = AURA_ENV || 'staging';
 
 export default config[ENV];
